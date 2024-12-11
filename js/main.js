01764854bca7d8a5b257f1b9c2b76aff31943614
Milana296 <55023441+Milana296@users.noreply.github.com>
2024-12-11 21:19:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+var swiper = new Swiper(".headerSwiper", {
+    spaceBetween: 155,
+    effect: "fade",
+    direction: "vertical",
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
